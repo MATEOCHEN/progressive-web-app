e88296f1158cbed886c64645267d857c6f21a233
Steven Chen <steven.chen@soohoobook.com>
2023-04-17 14:57:29 +0800

    update title

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <div className={styles.container}>
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://greenbox.tw/">Apple Store</a>
         </h1>
 
         <p className={styles.description}>
