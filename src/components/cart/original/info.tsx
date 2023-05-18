598052a9de98b2302f9f5d3bc2cf12ecb7be3153
Steven Chen <steven.chen@soohoobook.com>
2023-05-18 13:53:28 +0800

    make same day send notification

--- src/components/cart/original/info.tsx
+++ src/components/cart/original/info.tsx
@@ -7,7 +7,7 @@ const OriginalCartInfo = ({ ...props }: CartInfoProperties) => {
     <div className="cart-info flex flex-col items-center px-4 h-[100px] mb-4">
       <button
         className="default-anchor w-full my-4"
-        onClick={() => sendNotification("結帳成功! 您一共消費了3886元，我們會盡快送貨到府")}
+        onClick={() => sendNotification("結帳成功! 您一共消費了3886元，我們會盡快宅配到府")}
       >
         <div className="text-[#fff] w-full h-10 flex justify-center items-center bg-gb-green rounded-md">
           結帳
